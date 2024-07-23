51f96560594b3c51e39089e686ae2b5f76ce61e7
Santosh Yadav <santosh_yadav321@yahoo.com>
2024-07-23 21:43:21 +0530

    refactoring code

--- app/src/main/java/com/learning/roots/dsprogramsinkotlin/Anagram.kt
+++ app/src/main/java/com/learning/roots/dsprogramsinkotlin/Anagram.kt
@@ -5,7 +5,6 @@ package com.learning.roots.dsprogramsinkotlin
  */
 
 fun main() {
-
     val str1 = "anagram"
     val str2 = "managra"
 
@@ -13,7 +12,6 @@ fun main() {
         if (isAnagram(str1, str2)) "Anagram"
         else "Not Anagram"
     )
-
 }
 
 fun isAnagram(first: String, second: String): Boolean {
